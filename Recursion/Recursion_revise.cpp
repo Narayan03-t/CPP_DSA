751acf9e9b9221223026b53fca9810734fbc5cdf
Narayan03-t <guddu20768@gmail.com>
2026-02-25 01:34:49 +0530

    basic recursion practise lect 1

--- Recursion/Recursion_revise.cpp
+++ Recursion/Recursion_revise.cpp
@@ -2,63 +2,92 @@
 using namespace std;
 
 
-// void printNto1(int n){
-//     // base case
-//     if(n==0){
-//         return;
-//     }
-//     cout<<n<<" ";
-//     printNto1(n-1);
-// }
+int findFactorial(int n){
+    // base case
+    if(n==1){
+        return 1;
+    }
 
-// int findfactorial(int n){
-//     if(n==1){
-//         return 1;
-//     }
-//     int ans = n * findfactorial(n-1);
-    
-//     return ans;
-// }
+    // 1 case mai solve karung
+    int fact = n * findFactorial(n-1);
+    return fact;
 
-// void print1Ton(int n){
+}
 
 
-// }
-
-
-// void sumofdigit(int n){
-    
-// }
-
-
-void reverseString(string &str, int start, int end){
-    
-    if(start >= end){
+void printcountingNto1(int n){
+    // base case
+    if(n==0){
         return;
     }
-    
-    
-    swap(str[start], str[end]);
-    
-    
-    reverseString(str, start + 1, end - 1);
+
+    cout<<n<<" ";
+    printcountingNto1(n-1);
+
+}
+
+void printcounting1toN(int n){
+    if(n==0){
+        return;
+    }
+
+    printcounting1toN(n-1);
+    cout<<n<<" ";
+}
+
+
+int powof2(int n){
+    // base case
+    if(n==0){
+        return 1;
+    }
+
+    int ans = 2 * powof2(n-1);
+    return ans;
+}
+
+
+int findfibnocci(int n){
+    // base case
+    if(n==0 || n==1){
+        return n;
+    }
+
+    int ans = findfibnocci(n-1) + findfibnocci(n-2);
+    return ans;
+}
+
+
+int sumof1Ton(int n){
+    if(n==0){
+        return 0;
+    }
+
+    int ans = n + sumof1Ton(n-1);
+    return ans;
 }
 
 
 int main(){
+    // Find Factorial
+    // cout<<findFactorial(5);
 
-    string str;
-    cout << "Enter a string: ";
-    cin >> str;
-    
-    cout << "Original string: " << str << endl;
-    
-    
-    string str1 = str;
-    reverseString(str1, 0, str1.length() - 1);
-    cout << "Reversed " << str1 << endl;
-    
-    
+    // print counting  
+
+    // printcountingNto1(10);
+    // printcounting1toN(10);
+
+    // cout<<powof2(5);
+
+    //cout<<findfibnocci(8);
+
+    // for(int i=0;i<8;i++){
+    //     cout<<findfibnocci(i)<<" ";
+    // }
+
+
+    cout<<sumof1Ton(10);
+
 
     return 0;
 }
